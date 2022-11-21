9b0e80ee41da578a769fe082ac02c689df1a48ff
tabsa <tas.tiago.sousa@gmail.com>
2022-11-21 21:29:33 +0100

    Add fraud tables

--- databricks_poc_template/tasks/task_training.py
+++ databricks_poc_template/tasks/task_training.py
@@ -53,8 +53,10 @@ class TrainTask(Task):
 
         db_in = input_conf["database"]  
         raw_data_table = input_conf["raw_data_table"]  
+        enrich_1_table = input_conf["enrich1"]  
+        enrich_2_table = input_conf["enrich2"]  
         label_table = input_conf["label_table"] 
-        fs_schema = input_conf["fs_schema"] 
+        fs_db = input_conf["fs_database"] 
         fs_table = input_conf["fs_table"]          
 
         # Output
@@ -64,6 +66,8 @@ class TrainTask(Task):
         db_out = output_conf["database"]   
         train_dataset = output_conf["train_dataset"] 
         test_dataset = output_conf["test_dataset"]         
+        train_seed = output_conf['random_state']      
+        train_ratio = output_conf['train_size']  
 
         # Model configs
         model_conf = self.conf["model"]
@@ -75,7 +79,7 @@ class TrainTask(Task):
         mlflow.set_experiment(experiment) # Define the MLFlow experiment location
 
         # =======================
-        # 1. Loading the raw data
+        # 1. Loading the raw data (from feature store but let's discuss)
         # =======================
 
         # check this
@@ -84,22 +88,15 @@ class TrainTask(Task):
             self.logger.info(f"DBFS directory: {l}")           
 
         try:        
+            # Initialize the Feature Store client
+            fs = feature_store.FeatureStoreClient()
             # Load the raw data and associated label tables
             raw_data = spark.table(f"{db_in}.{raw_data_table}")
             labels = spark.table(f"{db_in}.{label_table}")
             
             # Joining raw_data and labels
-            raw_data_with_labels = raw_data.join(labels, ['Id','date','hour'])
-            display(raw_data_with_labels)
+            raw_data_with_labels = raw_data.join(labels, 'trxn_id')
             
-            # Selection of the data and labels until last LARGE time step (e.g. day or week let's say)
-            # Hence we will remove the last large timestep of the data
-            # max_hour = raw_data_with_labels.select("hour").rdd.max()[0]
-            max_date = raw_data_with_labels.select("date").rdd.max()[0]
-            print(max_date)
-            # raw_data_with_labels = raw_data_with_labels.withColumn("filter_out", when((col("hour")==max_hour) & (col("date")==max_date),"1").otherwise(0)) # don't take last hour of last day of data
-            raw_data_with_labels = raw_data_with_labels.withColumn("filter_out", when(col("date")==max_date,"1").otherwise(0)) # don't take last day of data
-            raw_data_with_labels = raw_data_with_labels.filter("filter_out==0").drop("filter_out")
             display(raw_data_with_labels)
             
             self.logger.info("Step 1.0 completed: Loaded historical raw data and labels")   
@@ -111,27 +108,28 @@ class TrainTask(Task):
             raise e  
 
         # ==================================
-        # 2. Building the training dataset
+        # 2. Building the training dataset (by adding enrichment layers)
         # ==================================
         try:        
-            # Initialize the Feature Store client
-            fs = feature_store.FeatureStoreClient()
-
             # Declaration of the features, in a "feature lookup" object
+            enrich_1_data = spark.table(f"{db_in}.{enrich_1_table}")
+            enrich_2_data = spark.table(f"{db_in}.{enrich_2_table}")
+            # Join enrichment tables
+            enrich_data = enrich_1_data.join(enrich_2_data, 'trxn_id')
             feature_lookups = [
                 FeatureLookup( 
-                table_name = f"{fs_schema}.{fs_table}",
-                feature_names = ["sl_norm","sw_norm","pl_norm","pw_norm"], # TODO: automate this in config file
-                lookup_key = ["Id","hour"], # TODO: automate this in config file
+                table_name = f"{fs_db}.{fs_table}",
+                feature_names = ["V21", "V22", "V23", "V24", 'V25', 'V26', 'V27', 'V28', 'Amount'],
+                lookup_key = 'trxn_id',
                 ),
             ]
 
             # Create the training dataset (includes the raw input data merged with corresponding features from feature table)
-            exclude_columns = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'Id', 'hour','date'] # TODO: should I exclude the 'Id', 'hour','date'? 
+            exclude_columns = ['trxn_id']
             training_set = fs.create_training_set(
                 df = raw_data_with_labels,
                 feature_lookups = feature_lookups,
-                label = "target",
+                label = "Class",
                 exclude_columns = exclude_columns
             )
 
@@ -145,21 +143,16 @@ class TrainTask(Task):
             data_pd = training_df.toPandas() #[features_and_label]
 
             # Do the train-test split
-            train, test = train_test_split(data_pd, train_size=0.7, random_state=123)
+            train, test = train_test_split(data_pd, train_size=train_ratio, random_state=train_seed)
             
             # Save train dataset
-            # train_pd = pd.DataFrame(data=np.column_stack((x_train,y_train)), columns=features_and_label)
-            # train_df = spark.createDataFrame(train_pd)
-            # train_df.write.format("delta").mode("overwrite").option("overwriteSchema", "true").saveAsTable(f"{db_out}.{train_dataset}")
             train_df = spark.createDataFrame(train)
-            train_df.write.format("delta").mode("overwrite").option("overwriteSchema", "true").saveAsTable(f"{db_out}.{train_dataset}")            
+            train_df.write.format("delta").mode("overwrite").option("overwriteSchema", "true").saveAsTable(f"{db_out}.{train_dataset}")                    
             
             # Save test dataset
-            # test_pd = pd.DataFrame(data=np.column_stack((x_test,y_test)), columns=features_and_label)
-            # test_df = spark.createDataFrame(test_pd)
             test_df = spark.createDataFrame(test)            
             test_df.write.format("delta").mode("overwrite").option("overwriteSchema", "true").saveAsTable(f"{db_out}.{test_dataset}") 
-    
+     
             self.logger.info("Step 2. completed: Building the training dataset")   
           
         except Exception as e:
@@ -187,9 +180,9 @@ class TrainTask(Task):
                                     param_grid=model_conf['hyperparameters_grid'],
                                     cv=5)
 
-                # Remove unneeded data
-                x_train = train.drop(["target"], axis=1)
-                y_train = train.target                
+                # Remove unneeded data (transaction id)
+                x_train = train.drop(["Class"], axis=1)
+                y_train = train.Class
                 # x_test = test.drop(["target"], axis=1)
                 # y_test = test.target
 
@@ -201,25 +194,32 @@ class TrainTask(Task):
                 model = CV_rfc.best_estimator_
 
                 # Tracking the model parameters
-                train_dataset_version = module.get_table_version(spark,f"{db_out}.{train_dataset}")
-                test_dataset_version = module.get_table_version(spark,f"{db_out}.{test_dataset}")
-                fs_table_version = module.get_table_version(spark,f"{fs_schema}.{fs_table}")
-                mlflow.set_tag("train_dataset_version", train_dataset_version)
-                mlflow.set_tag("test_dataset_version", test_dataset_version)
-                mlflow.set_tag("fs_table_version", fs_table_version)
-                mlflow.set_tag("train_dataset", f"{db_out}.{train_dataset}")
-                mlflow.set_tag("test_dataset", f"{db_out}.{test_dataset}")
-                mlflow.set_tag("raw_data", f"{db_in}.{raw_data_table}")
-                mlflow.set_tag("raw_labels", f"{db_in}.{label_table}")
+                train_version = module.get_table_version(spark,f"{db_out}.{train_dataset}")
+                test_version = module.get_table_version(spark,f"{db_out}.{test_dataset}")
+                fs_table_version = module.get_table_version(spark,f"{fs_db}.{fs_table}")
+                mlflow.set_tag("train_version", train_version)
+                mlflow.set_tag("test_version", test_version)
+                mlflow.set_tag("fs_table_version", fs_table_version) # Feature store version
+                mlflow.set_tag("train", f"{db_out}.{train_dataset}")
+                mlflow.set_tag("test", f"{db_out}.{test_dataset}")
+                mlflow.set_tag("raw_data", f"{db_in}.{raw_data_table}") # Fraud features base
+                mlflow.set_tag("raw_labels", f"{db_in}.{label_table}") # Fraud label
                 mlflow.set_tag("environment run", f"{env}") # Tag the environment where the run is done
                 signature = infer_signature(x_train, model.predict(x_train))  
 
                 # Add an random input example for the model
                 input_example = {
-                    "sepal_length": 5.1,
-                    "sepal_width": 3.5,
-                    "petal_length": 1.4,
-                    "petal_width": 0.2
+                    "V1": 0.1,
+                    "V2": -1.5,
+                    "V3": -1.4,
+                    "V4": 0.2,
+                    'V5': 0.3,
+                    'V6': 0.5,
+                    'V7': -0.2,
+                    'V8': 0.8,
+                    'V9': -1,
+                    'V10': 0.7,
+                    'Amount': 1000
                 }                               
                 
                 # Log the model
@@ -229,7 +229,7 @@ class TrainTask(Task):
                 fs.log_model(
                     model,
                     artifact_path=model_name,
-                    flavor=mlflow.sklearn,
+                    flavor=mlflow.xgboost,
                     training_set=training_set,
                     # registered_model_name=model_name,
                 )
